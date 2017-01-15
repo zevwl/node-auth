801c02f8dd037c935659f74795391dad3faf278d
zevwl <zevwl@bhphoto.com>
2017-01-15 15:58:11 -0500

    Minor changes

--- app/routes.js
+++ app/routes.js
@@ -5,25 +5,24 @@ const routerController = require('./controllers/router.controller');
 // Export router
 module.exports = router;
 
-
-// Lock the app to only allow authenticated users
-router.use(routerController.middleware);
-
-
-// route to show a random message (GET http://localhost:8080/api/)
-router.get('/', routerController.main);
-
-/**
- * API routes
- */
+//=======================
+// API routes
+//=======================
 
 // Setup route
 router.get('/setup', routerController.setup);
 
-router.get('/users', routerController.showUsers);
-
 router.get('/authenticate', routerController.authenticate);
 router.post('/authenticate', routerController.authenticate);
 
-// TODO: route middleware to verify a token
+//=======================
+// Everything form here down is locked
+//=======================
+router.use(routerController.middleware);
+
+// route to show a random message (GET http://localhost:8080/api/)
+router.get('/', routerController.main);
+
+// Get all users
+router.get('/users', routerController.showUsers);
 
